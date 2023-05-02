764f7a92e5d0f3283df6aa545f1db2f1ac187d24
veena gk <veenagkreddy8@gmail.com>
2023-05-02 13:51:36 +0530

    "removed some lazy loading code which was not working"

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -15,8 +15,6 @@ export class ProductsComponent implements OnInit {
   public visibleProducts: any;
   public imageUrl: any = {};
   emailId!: string;
-  
-  @ViewChild('imageContainer') imageContainer!: ElementRef;
 
   constructor(
     private router: Router,
@@ -24,54 +22,28 @@ export class ProductsComponent implements OnInit {
     private api: ApiService,
     private cartService: CartService,
     private http: HttpClient
-  ) {
+  ) 
+  {
     this.emailId = this.route.snapshot.queryParamMap.get('emailId');
   }
 
   ngOnInit(): void {
     this.api.getProduct().subscribe((res) => {
       this.productList = res;
-      this.visibleProducts = this.productList.slice(0, 10);
+      this.visibleProducts = this.productList.slice(0, 15);
       this.visibleProducts.forEach((a: any) => {
         Object.assign(a, { quantity: 1, total: a.price });
         this.fetchImage(a.id);
       });
     });
-
-    window.addEventListener('scroll', this.lazyLoadImages.bind(this));
   }
-
-  ngOnDestroy(): void {
-    window.removeEventListener('scroll', this.lazyLoadImages.bind(this));
-  }
-
   fetchImage(id: number): void {
     const url = `https://fakestoreapi.com/products/${id}`;
     this.http.get(url, { responseType: 'json' }).subscribe((data: any) => {
       this.imageUrl[id] = data.image;
     });
   }
-
   addtocart(item: any) {
     this.cartService.addtoCart(item);
-  }
-
-  lazyLoadImages() {
-    const container = this.imageContainer.nativeElement;
-    const images = container.querySelectorAll('.lazy-load-image');
-    const containerTop = container.getBoundingClientRect().top;
-
-    images.forEach((img: any) => {
-      if (
-        img.getBoundingClientRect().top - containerTop <
-        window.innerHeight * 2
-      ) {
-        const src = img.dataset.src;
-        if (src) {
-          img.setAttribute('src', src);
-          img.classList.remove('lazy-load-image');
-        }
-      }
-    });
   }
 }
